f16eba0b0a0b722921aa1342834a041714a79f75
Apurv Vinay Bidkar <97932925+apurvbidkar@users.noreply.github.com>
2023-01-04 16:20:18 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "us-east-1"
 <<<<<<< HEAD
-  access_key = "AKIAV4SSGKL6DDG5INHQ"
-  secret_key = "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"
+  access_key = "AKI5INHQ"
+  secret_key = "C+E6BY"
 =======
   access_key = "AKIAV4SS"
-  secret_key = "9qKerdr7I1/VCsXy"
+  secret_key = "9qKer1/VCsXy"
 >>>>>>> 7c8e0802a973473c52554dfd8f8a8e4328f55f35
 }
